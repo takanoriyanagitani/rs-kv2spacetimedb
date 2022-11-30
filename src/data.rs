cc48d811410555c9e98babb3bbd1c8de4287be01
yanagitani <takanori.yanagitani@gmail.com>
2022-12-01 08:00:56 +0900

    test: more tests

--- src/data.rs
+++ src/data.rs
@@ -35,3 +35,26 @@ impl<K, V> Data<K, V> {
 }
 
 pub type RawData = Data<Vec<u8>, Vec<u8>>;
+
+#[cfg(test)]
+mod test_data {
+
+    mod data_as_item {
+
+        use crate::{data::Data, date::Date, device::Device, item::Item};
+
+        #[test]
+        fn test_bytes() {
+            let dev: Device = Device::new_unchecked("cafef00ddeadbeafface864299792458".into());
+            let d: Date = Date::new_unchecked("2022_12_01".into());
+            let i: Item<_, _> = Item::new(b"07:58:48".to_vec(), b"42".to_vec());
+
+            let dat: Data<_, _> = Data::new(dev, d, i);
+            let ri: &Item<_, _> = dat.as_item();
+            let k: &[u8] = ri.as_key();
+            let v: &[u8] = ri.as_val();
+            assert_eq!(k, b"07:58:48");
+            assert_eq!(v, b"42");
+        }
+    }
+}
